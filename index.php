ee3d3fef64eb843a13cbfa70b884b949b9924410
Daniel Lopez <dannylo9891@gmail.com>
2014-07-15 23:06:29 -0500

    gruphabit
    Proyecto habitat

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Grupo Ambiental Hábitat" />
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"/>
+	<title>Grupo Ambiental Hábitat</title>
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/estilos.css" />
+	<script src="js/prefixfree.js"></script>
+</head>
+
+<body>
+
+	<header>
+		<div id="div_logo">
+			<figure id="logo">
+				<img src="img/habitat.png" />
+			</figure>
+			<h1>Grupo Ambiental Hábitat</h1>
+		</div>
+		<div id="div_menu">	
+			<nav>
+				<ul>
+					<li><a href="#">¿Quienes somos?</a></li>
+					<li><a href="#">Enlaces</a></li>
+					<li><a href="#">Logros</a></li>
+					<li><a href="#">Contactos</a></li>
+				</ul>
+			</nav>
+		</div>	
+	</header>
+
+	<section id="noticias"></section>
+
+	<section id="contenido">
+		<article class="post">
+			<div class="post-cajon">
+				<div class="post-contenido">
+					<p>Misión</p>
+					<p>Somos una empresa dedicada a proteger el medio ambiente, a través de métodos 
+						técnico-científicos y en la aplicación del marco jurídico, contribuyendo al 
+						éxito de nuestros clientes proporcionándoles soluciones integrales, con base 
+						en nuestro equipo técnico y humano perfectamente capacitado, motivado y comprometido.
+					</p>
+					<p>Visión</p>
+					<p>Mejorar y mantener el desarrollo sustentable, enfatizando en la protección del 
+						medio ambiente para optimizar los recursos naturales del país, consolidando la 
+						preferencia de nuestros clientes por la excelencia de los servicios ambientales 
+						que les proporcionamos.
+					</p>
+					<p>Política</p>
+					<p>El éxito de nuestra empresa depende de la preferencia y la satisfacción de nuestros 
+						clientes al ofrecerles soluciones con un nivel de calidad que rebase sus espectativas, 
+						por ello nos comprometemos a mejorar continuamente nuestra organizacion , así 
+						como a preservar nuestro medio ambiente.
+					</p>
+					<p>Valores</p>
+					<p>Estamos concientes de que para ofrecer un servicio de calidad así como aumentar 
+						nuestra productividad, es necesario fomentar valores de nuestro personal tales como:
+					</p>
+				</div>
+			</div>
+		</article>
+	</section>
+
+
+<footer id="footer" class="footer">
+    <div class="fcontenido">
+        <span>Grupo Ambiental Hábitat S. A. de C. V. © 2013</span>
+        <div class="social">
+            <div><span>Síguenos</span>
+            	<a href="" class="icon-twitter" target="_blank"></a>
+            	<a href="" class="icon-facebook"  target="_blank"></a>
+            	<a href="" class="icon-google-plus"  target="_blank"></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+</body>
+</html>
