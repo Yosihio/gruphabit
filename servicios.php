d869bc86320c60fe6f09dc2ae6b2df5c5d13f003
Daniel Lopez <dannylo9891@gmail.com>
2014-07-18 11:59:45 -0500

    Servicios

--- servicios.php
+++ servicios.php
@@ -43,25 +43,25 @@
 			<div class="post-cajon">
 				<div class="post-contenido">
 					<p id="tras"><strong>AMBIENTAL (UVPROFEPA 072) </strong></p>
-					<nav>
-						Manifiestos de Impacto Ambiental
-						Estudios de Riesgos
-						COA
-						LAU
-						LAF
-						Registro como empresa generadora de residuos peligrosos
-						Registro como empresa generadora de residuos de manejo especial
-						Estudios Técnicos Justificativos ante CONAFOR (Cambio de uso de suelo)
-						Planes de manejo de residuos
-						Atlas de Riesgos
-						Dictamen de Factibilidad Regional ante INVIVIENDA
-						Análisis de Riesgo y Vulnerabilidad
-						Programas internos de Protección Civil
-						Conformación de brigadas
-						Bitácoras de residuos peligrosos, no peligrosos y de manejo especial, volumen de aprovechamiento y descarga de agua
-						Programas de Rescate y Reubicación de Flora y Fauna
-						Programas de Prevención de Accidentes (PPA)
-					</nav>
+					<p>
+						-Manifiestos de Impacto Ambiental.<br>
+						-Estudios de Riesgos.<br>
+						-COA.<br>
+						-LAU.<br>
+						-LAF.<br>
+						-Registro como empresa generadora de residuos peligrosos.<br>
+						-Registro como empresa generadora de residuos de manejo especial.<br>
+						-Estudios Técnicos Justificativos ante CONAFOR (Cambio de uso de suelo).<br>
+						-Planes de manejo de residuos.<br>
+						-Atlas de Riesgos.<br>
+						-Dictamen de Factibilidad Regional ante INVIVIENDA.<br>
+						-Análisis de Riesgo y Vulnerabilidad.<br>
+						-Programas internos de Protección Civil.<br>
+						-Conformación de brigadas.<br>
+						-Bitácoras de residuos peligrosos, no peligrosos y de manejo especial, volumen de aprovechamiento y descarga de agua.<br>
+						-Programas de Rescate y Reubicación de Flora y Fauna.<br>
+						-Programas de Prevención de Accidentes (PPA).
+					</p>
 				</div>
 			</div>
 		</article>
@@ -72,12 +72,12 @@
 				<div class="post-contenido">
 					<p id="tras"><strong>MEDIO AMBIENTE LABORAL</strong></p>
 					<p>
-						Auditorias en materia de seguridad industrial para ser Empresa Segura. 
-						Capacitación en materia de Prevención y Combate de Incendios, Búsqueda y Rescate, Riesgos de trabajo, Manejo de Sustancias Químicas, Manejo de Residuos. 
-						Conformación de la comisión mixta de seguridad e higiene. 
-						Respuesta a actos de molestia o visitas de la STPS. 
-						Evaluaciones por NOM de la STPS. 
-						Estudios de grado de riesgo de incendio.
+						-Auditorias en materia de seguridad industrial para ser Empresa Segura.<br>
+						-Capacitación en materia de Prevención y Combate de Incendios, Búsqueda y Rescate, Riesgos de trabajo, Manejo de Sustancias Químicas, Manejo de Residuos.<br> 
+						-Conformación de la comisión mixta de seguridad e higiene.<br>
+						-Respuesta a actos de molestia o visitas de la STPS.<br>
+						-Evaluaciones por NOM de la STPS.<br>
+						-Estudios de grado de riesgo de incendio.
 					</p>
 				</div>
 			</div>
@@ -89,18 +89,18 @@
 				<div class="post-contenido">
 					<p id="tras"><strong>SEGURIDAD INDUSTRIAL Y MEDICINA DEL TRABAJO.</strong></p>
 					<p>
-						Medición de tierras físicas. 
-						Atlas de riesgos laborales. 
-						Estudio de Ruido Perimetral o Ruido Laboral. 
-						Estudio de grado de Iluminación en las áreas de trabajo. 
-						Programa anual de capacitación. 
-						Supervisiones técnicas de cumplimiento de la normatividad. 
-						Historias clínicas para los trabajadores. 
-						Medicina del trabajo. 
-						Conformación de la unidad local de protección civil. 
-						Verificación del uso del equipo de protección personal. 
-						Programas de identificación de riesgo en maquinaria y equipo. 
-						Estudio de riesgo laborales.
+						-Medición de tierras físicas.<br>
+						-Atlas de riesgos laborales.<br>
+						-Estudio de Ruido Perimetral o Ruido Laboral.<br>
+						-Estudio de grado de Iluminación en las áreas de trabajo.<br>
+						-Programa anual de capacitación.<br>
+						-Supervisiones técnicas de cumplimiento de la normatividad.<br>
+						-Historias clínicas para los trabajadores.<br>
+						-Medicina del trabajo.<br>
+						-Conformación de la unidad local de protección civil.<br>
+						-Verificación del uso del equipo de protección personal.<br>
+						-Programas de identificación de riesgo en maquinaria y equipo.<br>
+						-Estudio de riesgo laborales.
 					</p>
 				</div>
 			</div>
@@ -112,12 +112,12 @@
 				<div class="post-contenido">
 					<p id="tras"><strong>CALDERAS, RECIPIENTES SUJETOS A PRESIÓN Y CRIOGÉNICOS</strong></p>
 					<p>
-						Verificación de la NOM-020-STPS 2010 para recipientes sujetos a presión, calderas y recipientes criogénicos. 
-						Prueba hidrostática. 
-						Ensayos no destructivos. 
-						Elaboración de planos, manuales, bitácoras de los recipientes sujetos a presión, calderas y recipientes criogénicos. 
-						Capacitación en materia de operación de recipientes sujetos a presión. 
-						Tramite para la obtención de la Licencia de Funcionamiento.
+						-Verificación de la NOM-020-STPS 2010 para recipientes sujetos a presión, calderas y recipientes criogénicos.<br>
+						-Prueba hidrostática.<br>
+						-Ensayos no destructivos.<br>
+						-Elaboración de planos, manuales, bitácoras de los recipientes sujetos a presión, calderas y recipientes criogénicos.<br>
+						-Capacitación en materia de operación de recipientes sujetos a presión.<br>
+						-Tramite para la obtención de la Licencia de Funcionamiento.
 					</p>
 				</div>
 			</div>
@@ -129,13 +129,13 @@
 				<div class="post-contenido">
 					<p id="tras"><strong>LABORATORIO ACREDITADO ANTE EMA Y APROBADO POR CNA, SSA, STPS Y SEMARNAT</strong></p>
 					<p>
-						Muestreo y análisis de agua residual, potable, salobre y de mar. 
-						Evaluación de emisiones a la atmósfera en fuentes fijas. 
-						Evaluación del nivel de exposición del personal a agentes químicos y físicos en el medio ambiente laboral. 
-						Muestreo y análisis de residuos peligrosos (CRIT), lodos y biosólidos. 
-						Evaluación de la Calidad del Aire. 
-						Muestreo y análisis de suelos contaminados por hidrocarburos o metales. 
-						Muestreo y análisis en alimentos.
+						-Muestreo y análisis de agua residual, potable, salobre y de mar.<br>
+						-Evaluación de emisiones a la atmósfera en fuentes fijas.<br>
+						-Evaluación del nivel de exposición del personal a agentes químicos y físicos en el medio ambiente laboral.<br>
+						-Muestreo y análisis de residuos peligrosos (CRIT), lodos y biosólidos.<br>
+						-Evaluación de la Calidad del Aire.<br>
+						-Muestreo y análisis de suelos contaminados por hidrocarburos o metales.<br>
+						-Muestreo y análisis en alimentos.
 					</p>
 				</div>
 			</div>
